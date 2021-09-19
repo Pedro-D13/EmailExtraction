41a2e8b9463f5fd9c505e08a9fabd31e86198ed4
Pedro-D13 <p.afonsosd1994@gmail.com>
2021-09-19 14:33:19 +0100

    working naive approach to reading the file and then appending to a list

--- NewExtractor/src/Extractor.java
+++ NewExtractor/src/Extractor.java
@@ -2,24 +2,37 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Extractor {
     public static void main(String[] args) throws FileNotFoundException {
         fileReader();
     }
+
     private static void fileReader() {
         try {
-            Path filePath = Paths.get("Extractor/sample.txt");
+            Path filePath = Paths.get("sample.txt");
             String content = Files.readString(filePath);
-            System.out.println(content);
-        } catch (NoSuchFileException f) {
-            System.out.println("no such file go away and try again!!");
+            String[] split_content = content.split(" ");
+            System.out.println(split_content.length);
+            String ref = "@softwire.com";
+            ArrayList<String> foundEmails = new ArrayList<>();
+            for (String each_str : split_content) {
+
+                String resp = each_str.substring(0, each_str.length()).contains(ref) ? ref : "";
+                if (!resp.isEmpty()) {
+                    foundEmails.add(each_str);
+                }
+            }
+            System.out.println(foundEmails);
+            System.out.printf("Total Emails Found: %s", foundEmails.size());
         } catch (IOException e) {
-            e.printStackTrace();
+//            e.printStackTrace();
+            System.out.println("File not found");
         }
+
     }
 }
 
---
+++ NewExtractor/src/ReadingText.java
@@ -0,0 +1,5 @@
+public class ReadingText {
+    public static void main(String[] args) {
+        System.out.println("works");
+    }
+}
