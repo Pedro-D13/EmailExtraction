c9fe72997911b54ba9b481b30a08bd8122882ba3
Pedro-D13 <p.afonsosd1994@gmail.com>
2021-09-19 22:11:39 +0100

    working approach to pattern match the emails containing '@softwire.com'

--- NewExtractor/src/Extractor.java
+++ NewExtractor/src/Extractor.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 public class Extractor {
     public static void main(String[] args) throws FileNotFoundException {
@@ -13,26 +14,23 @@ public class Extractor {
 
     private static void fileReader() {
         try {
-            Path filePath = Paths.get("sample.txt");
-            String content = Files.readString(filePath);
-            String[] split_content = content.split(" ");
-            System.out.println(split_content.length);
-            String ref = "@softwire.com";
-            ArrayList<String> foundEmails = new ArrayList<>();
+            Path filePath = Paths.get("sample.txt"); // get sample text
+            String content = Files.readString(filePath); // read from the found file
+            String[] split_content = content.split(" "); // split the content of the file by space
+            System.out.println(split_content.length); // find the length
+            String ref = "@softwire.com"; // the string ref, of the sub string we are looking for
+            ArrayList<String> foundEmails = new ArrayList<>(); // create an array to store the found emails
             for (String each_str : split_content) {
-
-                String resp = each_str.substring(0, each_str.length()).contains(ref) ? ref : "";
-                if (!resp.isEmpty()) {
+                String result = each_str.substring(0, each_str.length()).contains(ref) ? ref : "";
+                if (!result.isEmpty()) { // if the result is not empty then we've found a string so append it to the array
                     foundEmails.add(each_str);
                 }
             }
             System.out.println(foundEmails);
             System.out.printf("Total Emails Found: %s", foundEmails.size());
         } catch (IOException e) {
-//            e.printStackTrace();
             System.out.println("File not found");
         }
-
     }
 }
 
